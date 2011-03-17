437e3d9bee30698003ae83e06deb4759368b7de9
Jérémy Subtil <jeremy.subtil@smile.fr>
2011-03-17 17:08:38 +0100

    Bootstrap: hello world.

---
+++ abitbol.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+from abitbol.abitbol import main
+
+main()
---
+++ abitbol/abitbol.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+'''
+Abitbol's main module
+
+@author: Jérémy Subtil <jeremy.subtil@smile.fr>
+'''
+
+def main():
+    print('Hello world!')
