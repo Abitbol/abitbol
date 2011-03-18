be015b5d6dd961034608d45740fd3dcee176debc
Jérémy Subtil <jeremy.subtil@smile.fr>
2011-03-18 11:46:46 +0100

    Simple connection to the XMPP server.

--- abitbol/abitbol.py
+++ abitbol/abitbol.py
@@ -1,9 +1,36 @@
 # -*- coding: utf-8 -*-
-'''
-Abitbol's main module
+"""
+Abitbol's main module.
+"""
 
-@author: Jérémy Subtil <jeremy.subtil@smile.fr>
-'''
+from xmpp import Client
+from xmpp import Message
+
+__author__ = "Jérémy Subtil"
+__email__ = "jeremy.subtil@smile.fr"
+
 
 def main():
-    print('Hello world!')
+
+	client = Client('localhost')
+
+	def message_handler(conn, mess_node):
+		client.send(Message('tim@localhost', 'reponse!'))
+
+	if not client.connect(server=('127.0.0.1', 5222)):
+		raise IOError('Can not connect to server.')
+
+	if not client.auth('abitbol', 'abitbol', 'abitbol'):
+		raise IOError('Can not auth with server.')
+
+	client.RegisterHandler('message', message_handler)
+
+	client.sendInitPresence()
+	
+	client.send(Message('tim@localhost', 'Test message'))
+
+	while 1:
+		client.Process(1)
+
+#	client.sendPresence(typ='unavailable')
+#	client.disconnect()
